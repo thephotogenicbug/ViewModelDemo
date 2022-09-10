0465242aed7764f2e9ff09926107f939d92ddd19
thephotogenicbug <navinkumarachari@gmail.com>
2022-09-10 22:51:56 +0530

    Build a Counter usingViewModel

---
+++ app/src/main/java/com/naveen/viewmodeldemo/MainActivityViewModel.kt
@@ -0,0 +1,11 @@
+package com.naveen.viewmodeldemo
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+    var count = 0
+
+    fun updateCount(){
+        ++count
+    }
+}
